6961e9cf09e5e4fb80d6512db239b7996966634e
Mathis Zeiher <mathis.zeiher@sick.de>
2022-06-14 11:33:05 +0200

    switched to context

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
@@ -47,10 +48,13 @@ func main() {
 			remoteConnection, err := net.DialTCP("tcp", nil, &net.TCPAddr{IP: net.ParseIP(dstHost), Port: dstPort})
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error establishing remote connection: %s\n", err)
+				srcConnection.Close()
+				return
 			}
-			forwarder := NewForwarder(*srcConnection, *remoteConnection)
 			defer remoteConnection.Close()
 			defer srcConnection.Close()
+
+			forwarder := NewForwarder(*srcConnection, *remoteConnection)
 
 			fmt.Fprintf(os.Stdout, "Started forwarding from %s to %s\n", srcConnection.LocalAddr().String(), remoteConnection.RemoteAddr().String())
 
@@ -62,47 +66,46 @@ func main() {
 }
 
 type Forwarder struct {
-	src        net.TCPConn
-	dst        net.TCPConn
-	errChannel chan error
-	stop       bool
+	src       net.TCPConn
+	dst       net.TCPConn
+	lastError error
 }
 
 func NewForwarder(src net.TCPConn, dst net.TCPConn) *Forwarder {
 	return &Forwarder{
-		src:        src,
-		dst:        dst,
-		stop:       false,
-		errChannel: make(chan error),
+		src:       src,
+		dst:       dst,
+		lastError: nil,
 	}
 }
 
 func (fw *Forwarder) Start() error {
-	go fw.Forward(&fw.src, &fw.dst)
-	go fw.Forward(&fw.dst, &fw.src)
-	err := <-fw.errChannel
-	fw.stop = true
-	return err
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	go fw.Forward(&fw.src, &fw.dst, ctx, cancelFunc)
+	go fw.Forward(&fw.dst, &fw.src, ctx, cancelFunc)
+	<-ctx.Done()
+	return fw.lastError
 }
 
-func (fw *Forwarder) Forward(in io.Reader, out io.Writer) {
+func (fw *Forwarder) Forward(in io.Reader, out io.Writer, ctx context.Context, cancel context.CancelFunc) {
 	buffer := make([]byte, 1024)
 	for {
 		read, err := in.Read(buffer)
 		if err != nil {
-			fw.errChannel <- err
-			break
+			fw.lastError = err
+			cancel()
+			return
 		}
 		_, err = out.Write(buffer[:read])
 		if err != nil {
-			select {
-			case fw.errChannel <- err:
-			default:
-			}
-			break
+			fw.lastError = err
+			cancel()
+			return
 		}
-		if fw.stop {
-			break
+		select {
+		case <-ctx.Done():
+			return
+		default:
 		}
 	}
 }
